061231392924f6ba889431eab09f2f562082a0c4
fabricio <fnmamao@gmail.com>
2020-05-22 09:31:32 -0300

    adiciona o exercicio

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MilhasParaKm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            int milhas;
+            double km;
+            Console.WriteLine("Entre com a medida em milhas:");
+            milhas = Convert.ToInt16(Console.ReadLine());
+            
+            km = (milhas / 0.62137);
+            Console.WriteLine("a medida em km é:" + km);
+
+        }
+    }
+}
